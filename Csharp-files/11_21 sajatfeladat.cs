847d5bbf9ca45d67e1a0f4f5f3789a5bf14e8776
Baranyai Noel Ákos <baranyai.noel.akos@blathy.info>
2024-12-05 14:33:15 +0100

    csharp project

---
+++ Csharp-files/11_21 sajatfeladat.cs
@@ -0,0 +1,47 @@
+﻿namespace OraiFeladat
+{
+    internal class Program
+    {
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+        static bool AddTwoNumbers(int num, int num2, out int number)
+        {
+            number =  num + num2;
+            return true;
+        }
+
+        static string IsODDorEVEN(int num)
+        {
+            if (num % 2 == 0) { return "Páros"; }
+            return "Páratlan";
+        }
+        static void Main(string[] args) {
+            SayHello("Géza");
+            Console.WriteLine($"{AddTwoNumbers(1, 2, out int number)}, {IsODDorEVEN(number)} ");
+
+            int[] nums = { 1, 60, 6, 8, 20, 53 };
+            Console.WriteLine(WhichOneIsBigger(nums));
+            Console.WriteLine(WhichOneIsSmaller(nums));
+        }
+
+        static int WhichOneIsBigger(int[] nums)
+        {
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (nums[i] > max) {  max = nums[i]; }
+            }
+            return max;
+        }
+        static int WhichOneIsSmaller(int[] nums)
+        {
+            int min = 1000000000;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min) { min = nums[i]; }
+            }
+            return min;
+        }
+    }
+}
